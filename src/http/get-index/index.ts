60fbd32f02193f3c66b43dddecaf43d27d4e657a
Bryan Black <38768803+blockryan@users.noreply.github.com>
2020-09-28 15:59:42 -0700

    Update index.ts

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -1,10 +1,11 @@
-import { render } from "./render.tsx";
+// import { render } from "./render.tsx";
 
 export async function handler() {
+  let body = 'henlo from deno 🦕'
   return {
     headers: {
       "content-type": "text/html; charset=utf8",
     },
-    body: 'henlo from deno 🦕',
+    body
   };
 }
